a1c2a6ad11e559f50b46e1a605d7fd9d105124db
FranVT <vazqueztfj@proton.me>
2024-10-23 09:50:02 -0600

    Corrections in directories

--- tries/runStart.sh
+++ tries/runStart.sh
@@ -8,7 +8,7 @@
 ## Start the for loop
 for var_cCL in 0.02; #0.06 0.1;
 do 
-for Nexp in $(seq 1 5);
+for Nexp in $(seq 1 2);
 do
 
 # Cifras significativas
@@ -229,11 +229,11 @@ echo -e "mkdir $info_name;" >> $file_name;
 echo -e "cd $info_name; mkdir dumps; cd dumps;" >> $file_name;
 echo -e "mkdir assembly; mkdir shear; cd ..; cd ..;" >> $file_name;
 echo -e "" >> $file_name;
-echo -e "mpirun -n ${nodes} /mnt/MD1200B/cferreiro/fbenavides/lammps-2Aug2023/src/lmp_mpi -in in.assembly.lmp -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var steps $steps -var tstep $tstep -var sstep $sstep -var Nave $Nave" >> $file_name;
+echo -e "mpirun -n ${nodes} /mnt/MD1200B/cferreiro/fbenavides/lammps-2Aug2023/src/lmp_mpi -in in.assembly.lmp -var temp $T -var damp $damp -var L $L -var NCL $N_CL -var NMO $N_MO -var seed1 $seed1 -var seed2 $seed2 -var seed3 $seed3 -var steps $steps -var tstep $tstep -var sstep $sstep -var Nave $Nave" -var Dir $info_name>> $file_name;
 echo -e "" >> $file_name;
 echo -e "\n End of Assembly simulation, start of Deformation simulation \n"
 echo -e "" >> $file_name;
-echo -e "mpirun -n ${nodes} /mnt/MD1200B/cferreiro/fbenavides/lammps-2Aug2023/src/lmp_mpi -in in.shear.lmp -var temp $T -var damp $damp -var tstep $tstep_defor -var sstep $sstep_defor -var shear_rate $shear_rate -var max_strain $max_strain -var Nstep_per_strain $Nstep_per_strain -var shear_it $shear_it -var Nsave $Nsave -var seed3 $seed3 -var Nave $Nave -var rlxT1 $relaxTime1 -var rlxT2 $relaxTime2 -var rlxT3 $relaxTime3 -var rlxT4 $relaxTime4" >> $file_name;
+echo -e "mpirun -n ${nodes} /mnt/MD1200B/cferreiro/fbenavides/lammps-2Aug2023/src/lmp_mpi -in in.shear.lmp -var temp $T -var damp $damp -var tstep $tstep_defor -var sstep $sstep_defor -var shear_rate $shear_rate -var max_strain $max_strain -var Nstep_per_strain $Nstep_per_strain -var shear_it $shear_it -var Nsave $Nsave -var seed3 $seed3 -var Nave $Nave -var rlxT1 $relaxTime1 -var rlxT2 $relaxTime2 -var rlxT3 $relaxTime3 -var rlxT4 $relaxTime4" -var Dir $info_name>> $file_name;
 echo -e "" >> $file_name;
 echo -e "\n End of Deformation simulation \n"
 echo -e "" >> $file_name;
